2c28f7eff98d1f918e77b89fb77ead18b2f1649a
Jack5 <jack.flint3@gmail.com>
2024-07-25 11:48:51 +0200

    fix: added missing API services to demonstrate web binding and VD-Pattern

---
+++ src/BookHaven.UI/BookHaven.UI.AspNetCore/API/Accounts/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using BookHaven.Accounts.Application.Interfaces;
+using BookHaven.Accounts.Application.Schema.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookHaven.UI.AspNetCore.API.Accounts
+{
+    [ApiController]
+    [Route("api/1.0/[controller]/")]
+    public class AccountController : ControllerBase
+    {
+        IAccountService AccountService { get; set; }
+
+        public AccountController(IAccountService accountService)
+        {
+            AccountService = accountService;
+        }
+
+        [HttpPost("/register")]
+        public Task Register([FromBody] RegistrationDto registrationDto)
+        {
+            return AccountService.RegisterAsync(registrationDto);
+        }
+
+        [HttpPost("/login")]
+        public Task Login([FromBody] AccountLoginDto loginDto)
+        {
+            return AccountService.LoginAsync(loginDto);
+        }
+
+        [HttpPatch("/account/phone/{number}")]
+        public Task Set2FA([FromRoute(Name = "number")] string number)
+        {
+            return AccountService.Enable2FA(number);
+        }
+
+        [HttpPatch("/account/address")]
+        public Task SetAddress([FromBody] AddressDto address)
+        {
+            return AccountService.EnablePurchases(address);
+        }
+    }
+}
---
+++ src/BookHaven.UI/BookHaven.UI.AspNetCore/API/Accounts/ShopController.cs
@@ -0,0 +1,32 @@
+﻿using BookHaven.Accounts.Application.Interfaces;
+using BookHaven.Accounts.Application.Schema.DTO;
+using BookHaven.Orders.Application.Shema.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookHaven.UI.AspNetCore.API.Accounts
+{
+    [ApiController]
+    [Route("api/1.0/[controller]/")]
+    public class ShopController : ControllerBase
+    {
+        IShopService ShopService { get; set; }
+
+        public ShopController(IShopService shopService)
+        {
+            ShopService = shopService;
+        }
+
+        [HttpPut("Basket/{ISBN}")]
+        public Task PutInBasket([FromRoute(Name = "ISBN")]string id) 
+        {
+            return ShopService.AddToBasketAsync(new BasketItemDto() { Amount = 1, Publication = new BookPublicationDto { ISBN = id } });
+        }
+
+        [HttpPost("Basket/Purachse")]
+        public Task Purchase()
+        {
+            return ShopService.PurchaseBooksAsync();
+        }
+    }
+}
